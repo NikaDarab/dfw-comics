fd6b580e29d40d450521d2db872a6fdae9dfa64e
Nikadarab <nikadarab@pm.me>
2023-07-06 15:25:20 -0500

    artist body got updated, and artist page also updated

--- src/app/artists.tsx
+++ src/app/artists.tsx
@@ -1,14 +1,15 @@
-interface Artist {
+interface Dates {
+  date: string;
+  time: string;
+  location: string;
+  ticketLink: string;
+}
+export interface Artist {
   id: number;
   name: string;
   bio: string;
   headShot: string;
-  dates: {
-    date: string;
-    time: string;
-    location: string;
-    ticketLink: string;
-  };
+  dates: Dates[];
   socialMedia: {
     instagram: string;
     facebook: string;
@@ -24,12 +25,20 @@ export const artists: Artist[] = [
     name: "John Smith",
     bio: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
     headShot: "/../public/dude.jpg",
-    dates: {
-      date: "July 6, 2023",
-      time: "8:30PM",
-      location: "Addison Improv",
-      ticketLink: "https://example.com/john-smith-tickets",
-    },
+    dates: [
+      {
+        date: "July 6, 2023",
+        time: "8:30PM",
+        location: "Addison Improv",
+        ticketLink: "https://example.com/john-smith-tickets",
+      },
+      {
+        date: "July 6, 2023",
+        time: "8:30PM",
+        location: "Addison Improv",
+        ticketLink: "https://example.com/john-smith-tickets",
+      },
+    ],
     socialMedia: {
       instagram: "https://instagram.com/johnsmith",
       facebook: "https://facebook.com/johnsmith",
@@ -42,13 +51,27 @@ export const artists: Artist[] = [
     id: 2,
     name: "Sarah Johnson",
     bio: "Sarah Johnson is a rising star in the comedy scene with her hilarious observations on everyday life.",
-    headShot: "http://placebeard.it/640/480",
-    dates: {
-      date: "July 6, 2023",
-      time: "8:30PM",
-      location: "Addison Improv",
-      ticketLink: "https://example.com/sarah-johnson-tickets",
-    },
+    headShot: "/../public/lady.jpeg",
+    dates: [
+      {
+        date: "July 6, 2023",
+        time: "8:30PM",
+        location: "Addison Improv",
+        ticketLink: "https://example.com/sarah-johnson-tickets",
+      },
+      {
+        date: "July 6, 2023",
+        time: "8:30PM",
+        location: "Addison Improv",
+        ticketLink: "https://example.com/john-smith-tickets",
+      },
+      {
+        date: "July 6, 2023",
+        time: "8:30PM",
+        location: "Addison Improv",
+        ticketLink: "https://example.com/john-smith-tickets",
+      },
+    ],
     socialMedia: {
       instagram: "https://instagram.com/sarahjohnson",
       facebook: "https://facebook.com/sarahjohnson",
@@ -61,13 +84,15 @@ export const artists: Artist[] = [
     id: 3,
     name: "Michael Davis",
     bio: "Michael Davis is a veteran comedian known for his quick wit and hilarious improvisational skills.",
-    headShot: "http://placebeard.it/640/480",
-    dates: {
-      date: "July 6, 2023",
-      time: "8:30PM",
-      location: "Addison Improv",
-      ticketLink: "https://example.com/michael-davis-tickets",
-    },
+    headShot: "/../public/dude2.jpeg",
+    dates: [
+      {
+        date: "July 6, 2023",
+        time: "8:30PM",
+        location: "Addison Improv",
+        ticketLink: "https://example.com/michael-davis-tickets",
+      },
+    ],
     socialMedia: {
       instagram: "https://instagram.com/michaeldavis",
       facebook: "https://facebook.com/michaeldavis",
@@ -80,13 +105,33 @@ export const artists: Artist[] = [
     id: 4,
     name: "Jessica Roberts",
     bio: "Jessica Roberts is a comedian known for her sharp observations on relationships and pop culture.",
-    headShot: "http://placebeard.it/640/480",
-    dates: {
-      date: "July 6, 2023",
-      time: "8:30PM",
-      location: "Addison Improv",
-      ticketLink: "https://example.com/jessica-roberts-tickets",
-    },
+    headShot: "/../public/lady2.jpeg",
+    dates: [
+      {
+        date: "July 6, 2023",
+        time: "8:30PM",
+        location: "Addison Improv",
+        ticketLink: "https://example.com/jessica-roberts-tickets",
+      },
+      {
+        date: "July 6, 2023",
+        time: "8:30PM",
+        location: "Addison Improv",
+        ticketLink: "https://example.com/john-smith-tickets",
+      },
+      {
+        date: "July 6, 2023",
+        time: "8:30PM",
+        location: "Addison Improv",
+        ticketLink: "https://example.com/john-smith-tickets",
+      },
+      {
+        date: "July 6, 2023",
+        time: "8:30PM",
+        location: "Addison Improv",
+        ticketLink: "https://example.com/john-smith-tickets",
+      },
+    ],
     socialMedia: {
       instagram: "https://instagram.com/jessicaroberts",
       facebook: "https://facebook.com/jessicaroberts",
@@ -97,15 +142,29 @@ export const artists: Artist[] = [
   },
   {
     id: 5,
-    name: "Jessica Roberts",
-    bio: "Jessica Roberts is a comedian known for her sharp observations on relationships and pop culture.",
-    headShot: "http://placebeard.it/640/480",
-    dates: {
-      date: "July 6, 2023",
-      time: "8:30PM",
-      location: "Addison Improv",
-      ticketLink: "https://example.com/jessica-roberts-tickets",
-    },
+    name: "Ali Wong",
+    bio: "Ali Wong is a comedian known for her sharp observations on relationships and pop culture.",
+    headShot: "/../public/lady3.jpeg",
+    dates: [
+      {
+        date: "July 6, 2023",
+        time: "8:30PM",
+        location: "Addison Improv",
+        ticketLink: "https://example.com/jessica-roberts-tickets",
+      },
+      {
+        date: "July 6, 2023",
+        time: "8:30PM",
+        location: "Addison Improv",
+        ticketLink: "https://example.com/john-smith-tickets",
+      },
+      {
+        date: "July 6, 2023",
+        time: "8:30PM",
+        location: "Addison Improv",
+        ticketLink: "https://example.com/john-smith-tickets",
+      },
+    ],
     socialMedia: {
       instagram: "https://instagram.com/jessicaroberts",
       facebook: "https://facebook.com/jessicaroberts",
@@ -116,15 +175,23 @@ export const artists: Artist[] = [
   },
   {
     id: 6,
-    name: "Jessica Roberts",
-    bio: "Jessica Roberts is a comedian known for her sharp observations on relationships and pop culture.",
-    headShot: "http://placebeard.it/640/480",
-    dates: {
-      date: "July 6, 2023",
-      time: "8:30PM",
-      location: "Addison Improv",
-      ticketLink: "https://example.com/jessica-roberts-tickets",
-    },
+    name: "Alice Porter",
+    bio: "Alice Porter is a comedian known for her sharp observations on relationships and pop culture.",
+    headShot: "/../public/lady4.jpeg",
+    dates: [
+      {
+        date: "July 6, 2023",
+        time: "8:30PM",
+        location: "Addison Improv",
+        ticketLink: "https://example.com/jessica-roberts-tickets",
+      },
+      {
+        date: "July 6, 2023",
+        time: "8:30PM",
+        location: "Addison Improv",
+        ticketLink: "https://example.com/john-smith-tickets",
+      },
+    ],
     socialMedia: {
       instagram: "https://instagram.com/jessicaroberts",
       facebook: "https://facebook.com/jessicaroberts",
@@ -135,15 +202,41 @@ export const artists: Artist[] = [
   },
   {
     id: 7,
-    name: "Jessica Roberts",
-    bio: "Jessica Roberts is a comedian known for her sharp observations on relationships and pop culture.",
-    headShot: "http://placebeard.it/640/480",
-    dates: {
-      date: "July 6, 2023",
-      time: "8:30PM",
-      location: "Addison Improv",
-      ticketLink: "https://example.com/jessica-roberts-tickets",
-    },
+    name: "Ahmed Ali",
+    bio: "Ahmed Ali is a comedian known for her sharp observations on relationships and pop culture.",
+    headShot: "/../public/dude4.jpeg",
+    dates: [
+      {
+        date: "July 6, 2023",
+        time: "8:30PM",
+        location: "Addison Improv",
+        ticketLink: "https://example.com/jessica-roberts-tickets",
+      },
+      {
+        date: "July 6, 2023",
+        time: "8:30PM",
+        location: "Addison Improv",
+        ticketLink: "https://example.com/john-smith-tickets",
+      },
+      {
+        date: "July 6, 2023",
+        time: "8:30PM",
+        location: "Addison Improv",
+        ticketLink: "https://example.com/john-smith-tickets",
+      },
+      {
+        date: "July 6, 2023",
+        time: "8:30PM",
+        location: "Addison Improv",
+        ticketLink: "https://example.com/john-smith-tickets",
+      },
+      {
+        date: "July 6, 2023",
+        time: "8:30PM",
+        location: "Addison Improv",
+        ticketLink: "https://example.com/john-smith-tickets",
+      },
+    ],
     socialMedia: {
       instagram: "https://instagram.com/jessicaroberts",
       facebook: "https://facebook.com/jessicaroberts",
@@ -154,53 +247,23 @@ export const artists: Artist[] = [
   },
   {
     id: 8,
-    name: "Jessica Roberts",
-    bio: "Jessica Roberts is a comedian known for her sharp observations on relationships and pop culture.",
-    headShot: "http://placebeard.it/640/480",
-    dates: {
-      date: "July 6, 2023",
-      time: "8:30PM",
-      location: "Addison Improv",
-      ticketLink: "https://example.com/jessica-roberts-tickets",
-    },
-    socialMedia: {
-      instagram: "https://instagram.com/jessicaroberts",
-      facebook: "https://facebook.com/jessicaroberts",
-      twitter: "https://twitter.com/jessicaroberts",
-      tiktok: "https://tiktok.com/@jessicaroberts",
-    },
-    email: "jessicaroberts@example.com",
-  },
-  {
-    id: 9,
-    name: "Jessica Roberts",
-    bio: "Jessica Roberts is a comedian known for her sharp observations on relationships and pop culture.",
-    headShot: "http://placebeard.it/640/480",
-    dates: {
-      date: "July 6, 2023",
-      time: "8:30PM",
-      location: "Addison Improv",
-      ticketLink: "https://example.com/jessica-roberts-tickets",
-    },
-    socialMedia: {
-      instagram: "https://instagram.com/jessicaroberts",
-      facebook: "https://facebook.com/jessicaroberts",
-      twitter: "https://twitter.com/jessicaroberts",
-      tiktok: "https://tiktok.com/@jessicaroberts",
-    },
-    email: "jessicaroberts@example.com",
-  },
-  {
-    id: 10,
-    name: "Jessica Roberts",
-    bio: "Jessica Roberts is a comedian known for her sharp observations on relationships and pop culture.",
-    headShot: "http://placebeard.it/640/480",
-    dates: {
-      date: "July 6, 2023",
-      time: "8:30PM",
-      location: "Addison Improv",
-      ticketLink: "https://example.com/jessica-roberts-tickets",
-    },
+    name: "Sam Asghari",
+    bio: "Sam Asghari is a comedian known for her sharp observations on relationships and pop culture.",
+    headShot: "/../public/dude3.jpeg",
+    dates: [
+      {
+        date: "July 6, 2023",
+        time: "8:30PM",
+        location: "Addison Improv",
+        ticketLink: "https://example.com/jessica-roberts-tickets",
+      },
+      {
+        date: "July 6, 2023",
+        time: "8:30PM",
+        location: "Addison Improv",
+        ticketLink: "https://example.com/john-smith-tickets",
+      },
+    ],
     socialMedia: {
       instagram: "https://instagram.com/jessicaroberts",
       facebook: "https://facebook.com/jessicaroberts",
--- src/app/artists/[...artist]/page.tsx
+++ src/app/artists/[...artist]/page.tsx
@@ -1,6 +1,9 @@
-import { artists } from "../../artists";
+import React from "react";
+import { nanoid } from "nanoid";
+import { artists, Artist } from "../../artists";
 import Image from "next/image";
-
+import { FaFacebook, FaInstagram, FaTwitter, FaTiktok } from "react-icons/fa";
+import Typewriter from "@/app/Typewriter";
 interface Props {
   params: {
     artist: string;
@@ -8,37 +11,74 @@ interface Props {
 }
 
 const ArtistPage: React.FC<Props> = ({ params }) => {
-  const artist = artists.find(
+  const artist: Artist | undefined = artists.find(
     (artist) => artist.name.split(" ").join("") == params.artist
   );
   return (
     <>
-      <div className="flex justify-center text-3xl my-9">{artist?.name}</div>
-      <div className="bio-container border">
-        <div className="img-container relative w-full overflow-hidden">
+      <div className="flex justify-center text-4xl my-9">
+        <Typewriter text={artist?.name ?? ""} />
+      </div>
+      <div className="bio-container mb-[10%]">
+        <div className="flex flex-col items-center justify-around relative w-full overflow-hidden h-[400px] img-container">
+          <div className="flex flex-col justify-evenly socials-container md:h-[400px] lg:h-[400px]">
+            <a target="_blank" href={artist?.socialMedia.facebook}>
+              <FaFacebook className="text-5xl" />
+            </a>
+            <a target="_blank" href={artist?.socialMedia.instagram}>
+              <FaInstagram className="text-5xl" />
+            </a>
+            <a target="_blank" href={artist?.socialMedia.twitter}>
+              <FaTwitter className="text-5xl" />
+            </a>
+            <a target="_blank" href={artist?.socialMedia.tiktok}>
+              <FaTiktok className="text-5xl" />
+            </a>
+          </div>
           <Image
             src={artist?.headShot ?? "/../src/app/dude.jpg"}
             alt={`${artist?.name} Headshot`}
             height="500"
             width="500"
-            className="border max-w-full h-auto block object-cover object-bottom"
+            className="max-w-full h-auto block object-cover object-bottom"
           />
         </div>
-        <div>
-          <div className="border bio">{artist?.bio}</div>
+        <div className="flex items-center bio">
+          <div className="text-3xl  flex justify-center leading-10">
+            {artist?.bio}
+          </div>
         </div>
       </div>
-      <div className="ticket-container flex my-9 border">
-        <div>{artist?.dates.location}</div>
-        <div>{artist?.dates.date}</div>
-        <div>{artist?.dates.time}</div>
-        <a
-          href={artist?.dates.ticketLink}
-          target="_blank"
-          className="ticket-button"
-        >
-          Ticket
-        </a>
+
+      <div className="grid grid-cols-3 mt-[10%] show-container">
+        <div className="col-span-3 flex justify-between items-center p-4">
+          <p className="text-4xl font-bold">Location</p>
+          <div className="flex gap-[5px]">
+            <p className="text-3xl font-bold">Date & Time</p>
+          </div>
+          <p className="text-4xl font-bold text-center">Tickets</p>
+        </div>
+        {artist?.dates.map((d) => (
+          <div key={nanoid()} className="col-span-3">
+            <div className="flex justify-between items-center p-4">
+              <p className="text-3xl">{d.location}</p>
+              <div className="flex">
+                <p className="text-2xl">
+                  {d.date},{d.time}
+                </p>
+              </div>
+              <div>
+                <a
+                  href={d.ticketLink}
+                  target="_blank"
+                  className="ticket-button inline-block bg-green-600 text-white no-underline rounded text-center cursor-pointer text-base font-bold uppercase"
+                >
+                  Ticket
+                </a>
+              </div>
+            </div>
+          </div>
+        ))}
       </div>
     </>
   );
